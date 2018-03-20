e75c99bd0e0c6276734baa1744259e27e823bcae
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-03-21 00:48:50 +0100

    Clean code

--- src/js/Counter.js
+++ src/js/Counter.js
@@ -6,22 +6,28 @@ import {Child} from './Child';
 
 export class Counter extends React.Component {
 	constructor() {
-		super(),
-		this.state = {value: 0} //ES6 getInitialState
+		super();
+		this.state = {value: 0}; //ES6 getInitialState
 		// this.inc = this.inc.bind(this)
 	}
 	// static defaultProps() {} //ES7 defaultProps
 	// getInitialState() {} //ES5
 	// componentWillMount() // Get initial config from external API in root component
+	// componentDidMount() // Everything what could be done to mounted component and loading data to component
+	// componentWillReciveProps() // Read new props and decide what to do them (eg. update old props on several conditions)
+	// shouldComponentUpdate() // Allow or block component re-render after reciving new props. Used for improving performance
+	// componentWillUpdate // Same as componentWillMount
+	// componentDidUpdate // Same as componentDidMount but due to prop changes
+	// componentWillUnmount() // Remove eventListeners or network requests. Cleaning up componentDidMount after unmounting whole component
 	inc() {
 		this.setState({
 			value: this.state.value + 1
-		})
+		});
 	}
 	dec() {
 		this.setState({
 			value: this.state.value - 1
-		})
+		});
 	}
 
 	render() {
@@ -34,12 +40,6 @@ export class Counter extends React.Component {
 			</div>
 		)
 	}
-	// componentDidMount() // Everything what could be done to mounted component and loading data to component
-	// componentWillReciveProps() // Read new props and decide what to do them (eg. update old props on several conditions)
-	// shouldComponentUpdate() // Allow or block component re-render after reciving new props. Used for improving performance
-	// componentWillUpdate // Same as componentWillMount
-	// componentDidUpdate // Same as componentDidMount but due to prop changes
-	// componentWillUnmount() // Remove eventListeners or network requests. Cleaning up componentDidMount after unmounting whole component
 }
 
 // Counter.defaultProps() = {}; // React 15.5^ defaultProps
--- src/js/Parent.js
+++ src/js/Parent.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import {Counter} from './Counter'
+import {Counter} from './Counter';
 
 
 class Parent extends React.Component {
@@ -16,4 +16,4 @@ class Parent extends React.Component {
 	}
 }
 
-ReactDOM.render(<Parent/>, document.getElementById('app'))
+ReactDOM.render(<Parent/>, document.getElementById('app'));
