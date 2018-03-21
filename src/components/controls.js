e6c63e4261b9189b7fcdedfea14137013edd3a68
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-03-21 02:54:18 +0100

    Clean upcode

--- src/components/controls.js
+++ src/components/controls.js
@@ -7,53 +7,53 @@ function pad0(value) {
 }
 
 class Stopwatch {
-    constructor(display) {
-        this.running = false;
-        this.display = display;
-        this.reset();
-        this.print(this.times);
-    }
-    reset() {
-    	this.times = {
-    		minutes: 0,
-    		seconds: 0,
-    		miliseconds: 0
-    	}
-    	this.print()
-    }
-    print() {
-    	this.display.innerText = this.format(this.times)
-    }
-    format(times) {
-    	return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
-    }
-    start() {
-    	if(!this.running) {
-    		this.running = true;
-    		this.watch = setInterval(() => this.step(), 10)
-    	}
-    }
-    step() {
-    	if(!this.running) return;
-    	this.calculate();
-    	this.print();
-    }
-    calculate() {
-    	this.times.miliseconds += 1;
-    	if (this.times.miliseconds >=100) {
-    		this.times.seconds += 1;
-    		this.times.miliseconds = 0;
-    	}
-    	if (this.times.seconds >= 60) {
-    		this.times.minutes += 1;
-    		this.times.seconds = 0;
-    	}
-    }
-    stop() {
-    	this.running = false;
-    	clearInterval(this.watch);
-    }
-    
+	constructor(display) {
+		this.running = false;
+		this.display = display;
+		this.reset();
+		this.print(this.times);
+	}
+	reset() {
+		this.times = {
+			minutes: 0,
+			seconds: 0,
+			miliseconds: 0
+		};
+		this.print();
+	}
+	print() {
+		this.display.innerText = this.format(this.times);
+	}
+	format(times) {
+		return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+	}
+	start() {
+		if(!this.running) {
+			this.running = true;
+			this.watch = setInterval(() => this.step(), 10);
+		}
+	}
+	step() {
+		if(!this.running) return;
+		this.calculate();
+		this.print();
+	}
+	calculate() {
+		this.times.miliseconds += 1;
+		if (this.times.miliseconds >=100) {
+			this.times.seconds += 1;
+			this.times.miliseconds = 0;
+		}
+		if (this.times.seconds >= 60) {
+			this.times.minutes += 1;
+			this.times.seconds = 0;
+		}
+	}
+	stop() {
+		this.running = false;
+		clearInterval(this.watch);
+	}
+	
 }
 
 const stopwatch = new Stopwatch(document.querySelector('.stopwatch'));
